d401a2ee097c626cb5332175cd8c42fc6d110ebd
Harm van de Kraats <78961607+HarmKraats@users.noreply.github.com>
2023-05-03 01:04:37 +0200

    Added Home Single type
    Added a home single type to fill in, with 3 components.
    
    Img slider
    Content
    Products
    
    Also Header and Button.

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,12 +6,37 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import api from '@/api'
+
+
+/*
+// TODO
+
+- Create components for the componentes we setup in strapi
+- Get all the data from Pagebuilder from the api
+- Loop through the data and render the components in the right order
+- Create a component for the header - also in strapi
+- Create a component for the footer - also in strapi
+- Create a component for the navigation - also in strapi
+
+
+
+*/
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
+  created() {
+    this.getTest()
+  },
+  methods: {
+    async getTest() {
+      const response = await api.get('/api/home?populate[Pagebuilder][populate]=*')
+      console.log(response)
+    }
+  },
 }
 </script>
