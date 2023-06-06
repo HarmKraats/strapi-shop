5b93ea7b29267d1c3e0e2bc9c78dd728c39d0f00
Harm van de Kraats <78961607+HarmKraats@users.noreply.github.com>
2023-06-06 17:08:44 +0200

    uninstalled strapi-stripe

---
+++ strapi/config/plugins.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'strapi-to-stripe': {
+      enabled: true,
+      resolve: './src/plugins/strapi-to-stripe'
+    },
+  }
---
+++ strapi/src/plugins/strapi-to-stripe/README.md
@@ -0,0 +1,3 @@
+# Strapi plugin strapi-to-stripe
+
+A quick description of strapi-to-stripe.
